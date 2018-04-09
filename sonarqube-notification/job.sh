74efe60c9461763de825447a3f30084790d13b90
Lars Gohr <larsgohr@gmail.com>
2018-04-09 09:33:53 +0200

    Add slack

--- sonarqube-notification/job.sh
+++ sonarqube-notification/job.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env sh
+echo -n 'New sonar result for ' > /output/slack_message
+gron sonar-results/result.json \
+| grep key \
+| awk '{split($0,a,"= "); print a[2]}' \
+| sed -e 's/;//g' \
+| sed -e 's/"//g' >> /output/slack_message
 
 gron sonar-results/result.json \
 | grep measures \
 | grep -v periods \
 | grep -v "\[\]" \
 | grep -v {} \
-| awk '{split($0,a,"]."); print a[2]}'
+| awk '{split($0,a,"]."); print a[2]}' \
+| sed -e 's/value =/:/g' \
+| tr -d '\n' \
+| sed -e 's/metric =/\n/g' \
+| sed -e 's/;//g' \
+| sed -e 's/"//g' >> /output/slack_message
